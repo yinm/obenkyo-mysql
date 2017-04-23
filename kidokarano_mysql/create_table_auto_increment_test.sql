8179f0014ed2c82d0a1dd54851a428919454c3d6
yinm <yinm.cminor9@gmail.com>
2017-04-23 15:20:18 +0900

    chapter6

---
+++ kidokarano_mysql/create_table_auto_increment_test.sql
@@ -0,0 +1,6 @@
+create table auto_increment_test
+(
+  a int auto_increment primary key
+  ,b varchar(10)
+)
+;
---
+++ kidokarano_mysql/create_table_default_date.sql
@@ -0,0 +1,5 @@
+create table default_day
+(
+  id int auto_increment primary_key
+  ,day datetime
+)
